d963017c5159df7ef9f81b5eddbaff591c0b996f
Erik Nygren <sten.erik.nygren@gmail.com>
2018-05-10 13:05:02 -0400

    CORS for PUT

--- app/admin.py
+++ app/admin.py
@@ -137,7 +137,7 @@ class ListenersHandler(BaseHandler):
 	def set_default_headers(self):
 		self.set_header("Access-Control-Allow-Origin", "*")
 		self.set_header("Access-Control-Allow-Headers", "x-requested-with")
-		self.set_header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS')
+		self.set_header('Access-Control-Allow-Methods', 'POST, PUT, GET, OPTIONS')
 
 	def get(self):
 
